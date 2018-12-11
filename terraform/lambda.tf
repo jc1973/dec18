6dd750f73ce96470b31a8f875b1dcea4ad108a74
John Carter <john_p_carter@yahoo.co.uk>
2018-12-11 18:01:30 +0000

    just the lambda function

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -14,12 +14,6 @@ resource "aws_lambda_function" "hello_world_lambda_function" {
   publish = true
 }
 
-resource "aws_lambda_permission" "allow_aws_api_gateway" {
-  statement_id   = "AllowExecutionFromApiGateway"
-  action         = "lambda:InvokeFunction"
-  function_name  = "${aws_lambda_function.hello_world_lambda_function.arn}"
-  principal      = "apigateway.amazonaws.com"
-}
 
 resource "aws_iam_role" "lambda_role" {
   name = "iamrole-lambda-${ var.project }${ var.environment }"
@@ -32,28 +26,10 @@ resource "aws_iam_role" "lambda_role" {
       "Principal": {
         "Service": "lambda.amazonaws.com"
       },
-      "Effect": "Allow"
+      "Effect": "Allow",
+      "Sid": ""
     }
   ]
-}
-EOF
-}
-
-resource "aws_iam_role_policy" "iam_policy_lambda_role" {
-  role = "${aws_iam_role.lambda_role.id}"
-  name = "iampolicy-lambda_role-${ var.project }${ var.environment }"
-  policy = <<EOF
-{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Action": [
-                "logs:*"
-            ],
-            "Effect": "Allow",
-            "Resource": "*"
-        }
-    ]
 }
 EOF
 }
@@ -72,10 +48,6 @@ output "aws_iam_role.lambda_role.create_date" {
 
 output "aws_iam_role.lambda_role.unique_id" {
   value = "${aws_iam_role.lambda_role.unique_id}"
-}
-
-output "aws_iam_role_policy.iam_policy_lambda_role.id" {
-  value = "${aws_iam_role_policy.iam_policy_lambda_role.id}"
 }
 
 output "aws_lambda_function.hello_world_lambda_function.id" {
@@ -100,8 +72,4 @@ output "aws_lambda_function.hello_world_lambda_function.qualified_arn" {
 
 output "aws_lambda_function.hello_world_lambda_function.version" {
   value = "${aws_lambda_function.hello_world_lambda_function.version}"
-}
-
-output "aws_lambda_permission.allow_aws_api_gateway.id" {
-  value = "${aws_lambda_permission.allow_aws_api_gateway.id}"
 }
